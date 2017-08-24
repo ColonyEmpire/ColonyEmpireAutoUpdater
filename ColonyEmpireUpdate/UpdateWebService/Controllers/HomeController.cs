c17667786601579bbb4423055078db9f87c851ab
bernhardsson.philip <bernhardsson.philip@gmail.com>
2017-08-24 20:19:01 +0200

    Added admin view to update mod

--- ColonyEmpireUpdate/UpdateWebService/Controllers/HomeController.cs
+++ ColonyEmpireUpdate/UpdateWebService/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using UpdateWebService.Models;
 
 namespace UpdateWebService.Controllers
 {
@@ -6,9 +11,40 @@ namespace UpdateWebService.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AdminLogin login)
+        {
+            if (login.Username == ConfigurationManager.AppSettings["adminUsername"] && login.Password == ConfigurationManager.AppSettings["adminPassword"])
+            {
+                UpdateCEZip(login.Files[0]);
+                ViewBag.Message = "Update complete";
+            }
+            else
+            {
+                ViewBag.Message = "Incorrect Username/Password";
+            }
+
+
+            
+            return View();
+        }
+
+        private void UpdateCEZip(HttpPostedFileBase httpPostedFileBase)
+        {
+            System.IO.File.Delete(System.Web.HttpContext.Current.Server.MapPath("~/Update/ColonyEmpire.zip"));
+            httpPostedFileBase.SaveAs(System.Web.HttpContext.Current.Server.MapPath("~/Update/ColonyEmpire.zip"));
+
+
+            UpdateVersion();
+        }
+
+        private void UpdateVersion()
+        {
+            var version = System.IO.File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath("~/Update/version.txt"));
+            System.IO.File.WriteAllText(System.Web.HttpContext.Current.Server.MapPath("~/Update/version.txt"), (int.Parse(version)+1).ToString());
         }
     }
 }
---
+++ ColonyEmpireUpdate/UpdateWebService/Models/AdminLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UpdateWebService.Models
+{
+    public class AdminLogin
+    {
+        public AdminLogin()
+        {
+            Files = new List<HttpPostedFileBase>();
+        }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<HttpPostedFileBase> Files { get; set; }
+    }
+}
