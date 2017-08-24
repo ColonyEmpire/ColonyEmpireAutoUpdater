5cd23e35e03a8311c2876c4f91f1484d333cac17
bernhardsson.philip <bernhardsson.philip@gmail.com>
2017-08-24 14:51:51 +0200

    Added main task todos

--- ColonyEmpireUpdate/AutoUpdater/Program.cs
+++ ColonyEmpireUpdate/AutoUpdater/Program.cs
@@ -10,6 +10,17 @@ namespace AutoUpdater
     {
         static void Main(string[] args)
         {
+            //Todo: Check that the file is in the right place
+
+
+            //Todo: Get existing mod version
+
+
+            //Todo: Send request to update web service to check newest release version
+
+
+            //Todo: Update if installed version is old
+
         }
     }
 }
